2cf68ad6ef176f0380c30b2fc65c3ef337f17f3c
Esotastic <chrishjimenez@gmail.com>
2018-06-21 21:00:30 -0400

    Fixed ID error from mongo
    If a user changes a character in a provided ID, but keeps the ID the same length, it will force-stop the server. Working on a fix now.

--- v10/middleware/index.js
+++ v10/middleware/index.js
@@ -5,29 +5,10 @@ var Comment = require("../models/comment");
 //all of the middleware goes here!
 var middlewareObj = {};
 
-middlewareObj.checkCommentOwnership = function(req, res, next) {
-    if(req.isAuthenticated()){
-      Comment.findById(req.params.comment_id, function(err, foundComment){
-        if(err){
-          res.redirect("back");
-        } else {
-          //does user own comment?
-          if(foundComment.author.id.equals(req.user._id)){
-            next();
-          } else {
-            res.redirect("back");
-          }
-        }
-      });
-    } else {
-      res.redirect("back");
-    }
-  }
-
 middlewareObj.checkCampgroundOwnership = function(req, res, next) {
     if(req.isAuthenticated()){
       Campground.findById(req.params.id, function(err, foundCampground){
-        if(err){
+        if(err || !foundCampground){
           req.flash("error", "Campground not found!");
           res.redirect("back");
         } else {
@@ -45,6 +26,28 @@ middlewareObj.checkCampgroundOwnership = function(req, res, next) {
       res.redirect("back");
     }
   }
+
+
+  middlewareObj.checkCommentOwnership = function(req, res, next) {
+      if(req.isAuthenticated()){
+        Comment.findById(req.params.comment_id, function(err, foundComment){
+          if(err){
+            res.redirect("back");
+          } else {
+            //does user own comment?
+            if(foundComment.author.id.equals(req.user._id)){
+              next();
+            } else {
+              req.flash("error", "You don't have permission to do that!");
+              res.redirect("back");
+            }
+          }
+        });
+      } else {
+        req.flash("error", "You need to be logged in to do that!");
+        res.redirect("back");
+      }
+    }
 
 
 middlewareObj.isLoggedIn = function(req, res, next){
--- v10/routes/comments.js
+++ v10/routes/comments.js
@@ -30,6 +30,7 @@ router.post("/", middleware.isLoggedIn, function(req, res){
     } else {
       Comment.create(req.body.comment, function(err, comment){
         if(err){
+          req.flash("error", "Something went wrong!");
           console.log(err);
         } else {
           //add username and id to comments
@@ -39,6 +40,7 @@ router.post("/", middleware.isLoggedIn, function(req, res){
           comment.save();
           campground.comments.push(comment);
           campground.save();
+          req.flash("success", "Comment successfully added.");
           res.redirect("/campgrounds/" + campground._id);
         }
       });
